7f64b59c4ef59c8a5b3db0b5dc76b0ccea4089d9
Joshua Brown <145138553+jbrown2006@users.noreply.github.com>
2024-07-11 16:00:03 +0100

    Update basicformBMI.py

--- Forms/basicformBMI.py
+++ Forms/basicformBMI.py
@@ -1,1 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, RadioField, SelectField
 
+class BasicForm(FlaskForm):
+    weight = StringField('Weight:')
+    weight_unit = SelectField('Unit', choices=[('kg', 'Kilograms'), ('lbs', 'Pounds')])
+    height = StringField('Height:')
+    height_unit = SelectField('Unit', choices=[('cm', 'Centimeters'), ('in', 'Inches')])
+    submit = SubmitField('Calculate BMR')
